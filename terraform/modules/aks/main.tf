d8802dbd916ebc5c4de37c91c0d4e8174edb5288
Thomas Enjalbert <thomas.enjalbert.pro@gmail.com>
2026-09-08 12:13:18 +0200

    fix: update service endpoint configuration for AKS subnet

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -10,7 +10,10 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
-  service_endpoints    = ["Microsoft.Storage"]
+
+  service_endpoint {
+    service = "Microsoft.Storage"
+  }
 }
 
 resource "azurerm_kubernetes_cluster" "aks" {
