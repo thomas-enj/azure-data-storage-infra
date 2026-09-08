e101f3a567b075b667d0408f4ea8da5653d5faab
Thomas Enjalbert <thomas.enjalbert.pro@gmail.com>
2026-09-08 16:17:28 +0200

    fix: update VM size for database node pool to Standard_D2s_v3

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -53,7 +53,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "db_pool" {
   name                  = "dbpool"
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
   
-  vm_size               = "Standard_D2s_v5"
+  vm_size               = "Standard_D2s_v3"
   
   node_count            = 1                  
   mode                  = "User"
